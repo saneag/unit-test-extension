2f270b3812e59098b8908a86bbdfd3004b5712ef
Alexandr Garstea <end51160@adobe.com>
2025-10-07 20:17:19 +0300

    feat: add placeholder test cases for each exported function

--- src/helpers/createTestFileHelpers.ts
+++ src/helpers/createTestFileHelpers.ts
@@ -4,6 +4,57 @@ import { getFileNameWithoutExtension } from "./fileNameHelpers";
 import { CreationFileError } from "../errors/CreationFileError";
 import { UnitTestHelperCommands } from "../constants/commands";
 
+const isFunction = (line: string) => {
+  const functionPattern = /^(export\s+)?(async\s+)?function\s+\w+/;
+  return functionPattern.test(line.trim());
+};
+
+const isArrowFunction = (line: string) => {
+  const arrowFunctionPattern =
+    /^(export\s+)?(const|let|var)\s+\w+\s*=\s*(async\s+)?\(/;
+  return arrowFunctionPattern.test(line.trim());
+};
+
+export const createTestFileContent = async (fileNameWithPath: string) => {
+  const testFileName = getFileNameWithoutExtension(fileNameWithPath);
+
+  const exportedFunctions: string[] = [];
+
+  const document = await vscode.workspace.openTextDocument(
+    vscode.Uri.file(fileNameWithPath)
+  );
+
+  const lines = document.getText().split("\n");
+  for (const line of lines) {
+    const trimmedLine = line.trim();
+    if (isFunction(trimmedLine) || isArrowFunction(trimmedLine)) {
+      const functionNameMatch = trimmedLine.match(
+        /export (function|const|let|var) (\w+)/
+      );
+      if (functionNameMatch && functionNameMatch[2]) {
+        exportedFunctions.push(functionNameMatch[2]);
+      }
+    }
+  }
+
+  if (exportedFunctions.length <= 1) {
+    return `describe("${testFileName}", () => {\n  it("should", () => {});\n})`;
+  }
+
+  let testFileContent = `describe("${testFileName}", () => {\n`;
+  for (const func of exportedFunctions) {
+    const isLastFunction =
+      func === exportedFunctions[exportedFunctions.length - 1];
+
+    testFileContent += `  describe("${func}", () => {\n    it("should", () => {});\n  });\n${
+      isLastFunction ? "" : "\n"
+    }`;
+  }
+  testFileContent += `})`;
+
+  return testFileContent;
+};
+
 export const createTestFile = async (
   fileNameWithPath: string,
   testFileNameWithPath: string
@@ -14,10 +65,7 @@ export const createTestFile = async (
       vscode.Uri.file(testFileDirectory)
     );
 
-    const testFileName = getFileNameWithoutExtension(fileNameWithPath);
-
-    const testFileContent = `describe("${testFileName}", () => {\n  it("should", () => {});\n})`;
-
+    const testFileContent = await createTestFileContent(fileNameWithPath);
     await vscode.workspace.fs.writeFile(
       vscode.Uri.file(testFileNameWithPath),
       Buffer.from(testFileContent, "utf8")
